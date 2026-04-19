e139b86061f145c23554059ef1df89aacff71a07
nshh123 <nshutisam61@gmail.com>
2026-04-20 00:54:01 +0200

    Enhanced UI & Added Features

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,7 @@ export default clerkMiddleware((auth, req) => {
     auth().protect();
   }
 });
-
+// hihi
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
